5d44e01b95dab54fc340571231cdaa44dce1a7e0
Jester35 <tarasenko.jester35@gmail.com>
2022-01-23 16:02:35 +0200

    upd2301

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,5 +1,6 @@
 import '../js/components/sliders';
 import '../js/components/progress';
 import '../js/components/accordions';
+import '../js/components/portfolio-tabs';
 // import '../js/components/secondslider';
 // console.log('components');
