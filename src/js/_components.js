56ce08456c7733dbbdd9aa84c708cbe2aaf63353
Jester35 <tarasenko.jester35@gmail.com>
2022-02-12 19:48:23 +0200

    1202

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,5 +2,6 @@ import '../js/components/sliders';
 import '../js/components/progress';
 import '../js/components/accordions';
 import '../js/components/portfolio-tabs';
+import '../js/components/selects';
 // import '../js/components/secondslider';
 // console.log('components');
--- src/js/_vendor.js
+++ src/js/_vendor.js
@@ -1,3 +1,4 @@
 import './vendor/focus-visible.js';
 import './vendor/graph-accordion.js';
+import './vendor/choices.min.js';
 // import './vendor/swiper.min.js';
